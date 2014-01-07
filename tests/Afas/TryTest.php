e6ec5252bfecd5d32bcc25386213e14e88614662
Youri <yvankoppen@kerckebosch.nl>
2014-01-07 15:15:52 +0100

    Added composer.json and a directory for PHPUnit.

---
+++ tests/Afas/TryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains TryTest class.
+ */
+
+namespace Afas;
+
+class TryTest extends \PHPUnit_Framework_TestCase {
+  /**
+   * Just a sample test method.
+   */
+  public function testOne() {
+    $this->assertEquals(1, 1);
+  }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ *
+ * This file should PHPUnit give access to afas classes.
+ */
+
+// @todo implement!
