2756a17a9f814662a7856ac5b864155ff80ef34d
MegaChriz <megachriz@hotmail.com>
2018-03-05 09:55:35 +0100

    Added test coverage for CountryManager.

---
+++ tests/Core/Locale/CountryManagerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Afas\Tests\Core\Locale;
+
+use Afas\Core\Locale\CountryManager;
+use Afas\Tests\TestBase;
+
+/**
+ * @coversDefaultClass \Afas\Core\Locale\CountryManager
+ * @group AfasCoreLocale
+ */
+class CountryManagerTest extends TestBase {
+
+  /**
+   * The country manager under test.
+   *
+   * @var \Afas\Core\Locale\CountryManager
+   */
+  private $manager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->manager = new CountryManager();
+  }
+
+  /**
+   * @covers ::getList
+   */
+  public function testGetList() {
+    $this->assertInternalType('array', $this->manager->getList());
+  }
+
+  /**
+   * @covers ::getListNum3toIso2
+   */
+  public function testGetListNum3toIso2() {
+    $this->assertInternalType('array', $this->manager->getListNum3toIso2());
+  }
+
+  /**
+   * @covers ::getListFromCsv
+   */
+  public function testGetListFromCsv() {
+    $list = $this->manager->getListFromCsv();
+    $this->assertInternalType('array', $list);
+
+    // Assert keys.
+    $expected = [
+      'coid',
+      'name',
+      'eu',
+      'iso-alpha2',
+      'iso-alpha3',
+      'iso-num3',
+    ];
+    $this->assertEquals($expected, array_keys($list[0]));
+  }
+
+}
