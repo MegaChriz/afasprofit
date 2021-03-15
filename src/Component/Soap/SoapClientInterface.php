ea7ca7799d6ef3f97e9ea0b7c1ca72f14370b2ce
Bob Olde Hampsink <bob@robuust.digital>
2021-03-15 11:25:39 +0100

    Allow both PHP7 and PHP8

--- src/Component/Soap/SoapClientInterface.php
+++ src/Component/Soap/SoapClientInterface.php
@@ -2,68 +2,140 @@
 
 namespace Afas\Component\Soap;
 
-/**
- * Provides an interface for handling Soap Requests.
- */
-interface SoapClientInterface {
+if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
 
   /**
-   * Performs A SOAP request.
-   *
-   * {@inheritdoc}
+   * Provides an interface for handling Soap Requests.
    */
-  public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false);
+  interface SoapClientInterface {
+
+    /**
+     * Performs A SOAP request.
+     *
+     * {@inheritdoc}
+     */
+    public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false);
+
+    /**
+     * Returns list of available SOAP functions.
+     *
+     * @return array
+     */
+    public function __getFunctions();
+
+    /**
+     *
+     */
+    public function __getLastRequest();
+
+    /**
+     *
+     */
+    public function __getLastRequestHeaders();
+
+    /**
+     *
+     */
+    public function __getLastResponse();
+
+    /**
+     *
+     */
+    public function __getLastResponseHeaders();
+
+    /**
+     *
+     */
+    public function __getTypes();
+
+    /**
+     *
+     */
+    public function __setCookie(string $name, ?string $value = null);
+
+    /**
+     *
+     */
+    public function __setLocation(string $location);
+
+    /**
+     *
+     */
+    public function __setSoapHeaders($headers = null);
+
+    /**
+     *
+     */
+    public function __soapCall(string $name, array $args, ?array $options = null, $inputHeaders = null, &$outputHeaders = null);
+
+  }
+
+} else {
 
   /**
-   * Returns list of available SOAP functions.
-   *
-   * @return array
+   * Provides an interface for handling Soap Requests.
    */
-  public function __getFunctions();
+  interface SoapClientInterface {
 
-  /**
-   *
-   */
-  public function __getLastRequest();
+    /**
+     * Performs A SOAP request.
+     *
+     * {@inheritdoc}
+     */
+    public function __doRequest($request, $location, $action, $version, $one_way = 0);
 
-  /**
-   *
-   */
-  public function __getLastRequestHeaders();
+    /**
+     * Returns list of available SOAP functions.
+     *
+     * @return array
+     */
+    public function __getFunctions();
 
-  /**
-   *
-   */
-  public function __getLastResponse();
+    /**
+     *
+     */
+    public function __getLastRequest();
 
-  /**
-   *
-   */
-  public function __getLastResponseHeaders();
+    /**
+     *
+     */
+    public function __getLastRequestHeaders();
 
-  /**
-   *
-   */
-  public function __getTypes();
+    /**
+     *
+     */
+    public function __getLastResponse();
 
-  /**
-   *
-   */
-  public function __setCookie(string $name, ?string $value = null);
+    /**
+     *
+     */
+    public function __getLastResponseHeaders();
 
-  /**
-   *
-   */
-  public function __setLocation(string $location);
+    /**
+     *
+     */
+    public function __getTypes();
 
-  /**
-   *
-   */
-  public function __setSoapHeaders($headers = null);
+    /**
+     *
+     */
+    public function __setCookie($name, $value);
 
-  /**
-   *
-   */
-  public function __soapCall(string $name, array $args, ?array $options = null, $inputHeaders = null, &$outputHeaders = null);
+    /**
+     *
+     */
+    public function __setLocation($new_location);
+
+    /**
+     *
+     */
+    public function __setSoapHeaders($soapheaders);
+
+    /**
+     *
+     */
+    public function __soapCall($function_name, $arguments, $options = [], $input_headers = NULL, &$output_headers = NULL);
+
+  }
 
 }
