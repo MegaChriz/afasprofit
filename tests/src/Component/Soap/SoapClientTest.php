2fbd869c63307641949dc3c828e16d688613bce9
MegaChriz <megachriz@hotmail.com>
2021-11-30 10:57:57 +0100

    Added minimal test coverage for SoapClient class.

---
+++ tests/src/Component/Soap/SoapClientTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Afas\Tests\Component\Soap;
+
+use Afas\Component\Soap\SoapClient;
+use Afas\Tests\TestBase;
+
+/**
+ * @coversDefaultClass \Afas\Component\Soap\SoapClient
+ * @group AfasComponentUtility
+ */
+class SoapClientTest extends TestBase {
+
+  /**
+   * Tests that a SoapClient object can be created.
+   *
+   * In PHP 8, PHP's built in SoapClient got return types.
+   */
+  public function testConstruct() {
+    $client = new SoapClient(NULL, [
+      'location' => '',
+      'uri' => 'https://www.example.com/profitservices',
+      'trace' => 1,
+      'style' => SOAP_RPC,
+      'use' => SOAP_ENCODED,
+    ]);
+    $this->assertInstanceof(SoapClient::class, $client);
+  }
+
+}
