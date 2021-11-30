da0b59d3949c8f02d649e5a3016f5f670ede6ea2
Bob Olde Hampsink <bob@robuust.digital>
2021-11-30 11:06:31 +0100

    Updated to work with PHP8's SoapClient.

--- src/Component/Soap/SoapClient.php
+++ src/Component/Soap/SoapClient.php
@@ -4,17 +4,39 @@ namespace Afas\Component\Soap;
 
 use SoapClient as SoapClientBase;
 
-/**
- * A child of SoapClient that exist to just implement the interface.
- */
-class SoapClient extends SoapClientBase implements SoapClientInterface {
+if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
 
   /**
-   * Override of SoapClient::__doRequest().
+   * A child of SoapClient that exist to just implement the interface.
    */
-  public function __doRequest($request, $location, $action, $version, $one_way = 0) {
-    $request = preg_replace('/<(ns1\:[a-z0-9\:\ ]*)>/i', '<${1} xmlns="' . $this->uri . '">', $request);
-    return parent::__doRequest($request, $location, $action, $version, $one_way);
+  class SoapClient extends SoapClientBase implements SoapClientInterface {
+
+    /**
+     * Override of SoapClient::__doRequest().
+     */
+    public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false) {
+      $request = preg_replace('/<(ns1\:[a-z0-9\:\ ]*)>/i', '<${1} xmlns="' . $this->uri . '">', $request);
+      return parent::__doRequest($request, $location, $action, $version, $oneWay);
+    }
+
+  }
+
+}
+else {
+
+  /**
+   * A child of SoapClient that exist to just implement the interface.
+   */
+  class SoapClient extends SoapClientBase implements SoapClientInterface {
+
+    /**
+     * Override of SoapClient::__doRequest().
+     */
+    public function __doRequest($request, $location, $action, $version, $one_way = 0) {
+      $request = preg_replace('/<(ns1\:[a-z0-9\:\ ]*)>/i', '<${1} xmlns="' . $this->uri . '">', $request);
+      return parent::__doRequest($request, $location, $action, $version, $one_way);
+    }
+
   }
 
 }
