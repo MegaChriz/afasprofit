1048d92bb0a38ce4aca9f4e0d536efb0c32da440
Gijs Stegehuis <gijs@robuust.digital>
2026-03-03 10:28:44 +0100

    Fix PHP 8.5 compatibility

--- src/Component/Soap/SoapClientInterface.php
+++ src/Component/Soap/SoapClientInterface.php
@@ -2,7 +2,86 @@
 
 namespace Afas\Component\Soap;
 
-if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
+if (version_compare(PHP_VERSION, '8.5.0') >= 0) {
+
+  /**
+   * Provides an interface for handling Soap Requests.
+   */
+  interface SoapClientInterface {
+
+    /**
+     * Performs A SOAP request.
+     *
+     * {@inheritdoc}
+     */
+    public function __doRequest(string $request, string $location, string $action, int $version, bool $oneWay = false, ?string $uriParserClass = null): ?string;
+
+    /**
+     * Returns list of available SOAP functions.
+     *
+     * @return array
+     */
+    public function __getFunctions();
+
+    /**
+     * Returns the SOAP request XML from the most recent request.
+     *
+     * @return string|null
+     */
+    public function __getLastRequest();
+
+    /**
+     * Returns the SOAP request headers from the most recent request.
+     *
+     * @return string|null
+     */
+    public function __getLastRequestHeaders();
+
+    /**
+     * Returns the SOAP response XML from the most recent request.
+     *
+     * @return string|null
+     */
+    public function __getLastResponse();
+
+    /**
+     * Returns the SOAP response headers from the most recent request.
+     *
+     * @return string|null
+     */
+    public function __getLastResponseHeaders();
+
+    /**
+     * Returns list of SOAP types available in the WSDL.
+     *
+     * @return array
+     */
+    public function __getTypes();
+
+    /**
+     * Defines a cookie to be sent with SOAP requests.
+     */
+    public function __setCookie(string $name, ?string $value = null);
+
+    /**
+     * Sets the endpoint URL for SOAP requests.
+     */
+    public function __setLocation(string $location);
+
+    /**
+     * Sets SOAP headers to be sent with SOAP requests.
+     */
+    public function __setSoapHeaders($headers = null);
+
+    /**
+     * Calls a SOAP function.
+     */
+    public function __soapCall(string $name, array $args, ?array $options = null, $inputHeaders = null, &$outputHeaders = null);
+
+  }
+
+}
+elseif (version_compare(PHP_VERSION, '8.0.0') >= 0) {
 
   /**
    * Provides an interface for handling Soap Requests.
